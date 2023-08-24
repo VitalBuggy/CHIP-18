d2080edf4baaa057ecf67b6ca69bd4f35222f94e
vitalbuggy <huseyinemreozden73@gmail.com>
2023-08-24 22:33:20 +0300

    Added tables

--- core/Chip8.cpp
+++ core/Chip8.cpp
@@ -25,6 +25,20 @@ public:
     uint32_t display[64 * 32]{};
     uint16_t opcode;
 
+    // tables
+    typedef void (Chip8::*Chip8Func)();
+	Chip8Func table[0xF + 1];
+	Chip8Func table0[0xE + 1];
+	Chip8Func table8[0xE + 1];
+	Chip8Func tableE[0xE + 1];
+	Chip8Func tableF[0x65 + 1];
+
+    void T0();
+    void T8();
+    void TE();
+    void TF();
+    void OP_NULL();
+
     void LoadROM(char const *filename);
 
     void Cycle();
@@ -75,6 +89,59 @@ Chip8::Chip8() : randGen(std::chrono::system_clock::now().time_since_epoch().cou
     randByte = std::uniform_int_distribution<uint8_t>(0, 255U);
 
     FontSet f;
+
+    table[0x0] = &Chip8::T0;
+    table[0x1] = &Chip8::OP_1nnn;
+    table[0x2] = &Chip8::OP_2nnn;
+    table[0x3] = &Chip8::OP_3xkk;
+    table[0x4] = &Chip8::OP_4xkk;
+    table[0x5] = &Chip8::OP_5xy0;
+    table[0x6] = &Chip8::OP_6xkk;
+    table[0x7] = &Chip8::OP_7xkk;
+    table[0x8] = &Chip8::T8;
+    table[0x9] = &Chip8::OP_9xy0;
+    table[0xA] = &Chip8::OP_Annn;
+    table[0xB] = &Chip8::OP_Bnnn;
+    table[0xC] = &Chip8::OP_Cxnn;
+    table[0xD] = &Chip8::OP_Dxyn;
+    table[0xE] = &Chip8::TE;
+    table[0xF] = &Chip8::TF;
+
+    for (size_t i = 0; i <= 0xE; i++) {
+        table0[i] = &Chip8::OP_NULL;
+        table8[i] = &Chip8::OP_NULL;
+        tableE[i] = &Chip8::OP_NULL;
+    }
+
+    table0[0x0] = &Chip8::OP_00E0;
+    table0[0xE] = &Chip8::OP_00EE;
+
+    table8[0x0] = &Chip8::OP_8xy0;
+    table8[0x1] = &Chip8::OP_8xy1;
+    table8[0x2] = &Chip8::OP_8xy2;
+    table8[0x3] = &Chip8::OP_8xy3;
+    table8[0x4] = &Chip8::OP_8xy4;
+    table8[0x5] = &Chip8::OP_8xy5;
+    table8[0x6] = &Chip8::OP_8xy6;
+    table8[0x7] = &Chip8::OP_8xy7;
+    table8[0xE] = &Chip8::OP_8xyE;
+
+    tableE[0x1] = &Chip8::OP_ExA1;
+    tableE[0xE] = &Chip8::OP_Ex9E;
+
+    for (size_t i = 0; i <= 0x65; i++) {
+        tableF[i] = &Chip8::OP_NULL;
+    }
+
+    tableF[0x07] = &Chip8::OP_Fx07;
+    tableF[0x0A] = &Chip8::OP_Fx0A;
+    tableF[0x15] = &Chip8::OP_Fx15;
+    tableF[0x18] = &Chip8::OP_Fx18;
+    tableF[0x1E] = &Chip8::OP_Fx1E;
+    tableF[0x29] = &Chip8::OP_Fx29;
+    tableF[0x33] = &Chip8::OP_Fx33;
+    tableF[0x55] = &Chip8::OP_Fx55;
+    tableF[0x65] = &Chip8::OP_Fx65;
 
     for (unsigned int i = 0; i < f.FONTSET_SIZE; i++)
     {
@@ -449,7 +516,33 @@ void Chip8::OP_Fx65() // LD Vx, [I]
     }
 }
 
+void Chip8::T0() {
+    ((*this).*(this->table0[this->opcode & 0x000Fu]))();
+}
+
+void Chip8::T8() {
+    ((*this).*(this->table8[this->opcode & 0x000Fu]))();
+}
+
+void Chip8::TE() {
+    ((*this).*(this->tableE[this->opcode & 0x000Fu]))();
+}
+
+void Chip8::TF() {
+    ((*this).*(this->tableF[this->opcode & 0x000Fu]))();
+}
+
+void Chip8::OP_NULL() { }
+
 void Chip8::Cycle()
 {
+    this->opcode = (this->memory[this->pc] << 8u | this->memory[this->pc + 1]);
+    
+    this->pc += 2;
+    
+    // evil pointer hack
+    ((*this).*(this->table[(this->opcode & 0xf000u) >> 12u]))();
 
+    if (this->dtimer > 0) --this->dtimer;
+    if (this->stimer > 0) --this->stimer;
 }
